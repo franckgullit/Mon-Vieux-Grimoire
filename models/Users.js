2339b0209628d4a9ebb69bba9b940da382a6d5d8
franck gullit <frankiegullit@mail.com>
2024-09-23 15:58:08 +0200

    still not working

--- models/Users.js
+++ models/Users.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 
 const uniqueValidator = require('mongoose-unique-validator');
 
+const validateEmail = function (email) {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
+    return re.test(email.trim());
+};
+
 const userSchema = mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [validateEmail, "L'adresse mail renseignée n'est pas valide"],
+    },
+    
+    password: {type: String,required: true},
 });
 
 userSchema.plugin(uniqueValidator);
